2eeb26e0904ef0b8fc61e830c8a8f5e9feb04fdc
misbat <misbat.dev@gmail.com>
2022-12-21 21:59:55 +0100

    add utility methods to find transitively referenced assembly meta data references

---
+++ Extensions/AssemblyExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+
+namespace Misbat.CodeAnalysis.Test.Extensions;
+
+public static class AssemblyExtensions
+{
+    public static Assembly FindTransitivelyReferenced(this Assembly target, string name)
+        => FindTransitivelyReferenced(target, a => a.GetName().Name == name);
+
+    private static Assembly FindTransitivelyReferenced(this Assembly target, Predicate<Assembly> predicate)
+    {
+        var consumedAssemblies = new HashSet<string>();
+        var assembliesToCheck = new Queue<Assembly>();
+
+        assembliesToCheck.Enqueue(target);
+
+        while (assembliesToCheck.Count > 0)
+        {
+            Assembly assemblyToCheck = assembliesToCheck.Dequeue();
+            AssemblyName[] referencedAssemblies = assemblyToCheck.GetReferencedAssemblies();
+            foreach (AssemblyName referencedAssembly in referencedAssemblies)
+            {
+                string assemblyName = referencedAssembly.FullName;
+                Assembly assembly = Assembly.Load(referencedAssembly);
+
+                if (predicate(assembly))
+                {
+                    return assembly;
+                }
+
+                if (!consumedAssemblies.Contains(assemblyName))
+                {
+                    consumedAssemblies.Add(assemblyName);
+                    assembliesToCheck.Enqueue(assembly);
+                }
+            }
+        }
+
+        throw new ArgumentException($"Assembly with name '{target.GetName()}' does not transitively reference any assembly that matches the predicate.");
+    }
+}
--- Utility/MetadataReferenceUtility.cs
+++ Utility/MetadataReferenceUtility.cs
@@ -1,26 +1,46 @@
-﻿using JetBrains.Annotations;
+﻿using System.Reflection;
+using JetBrains.Annotations;
 using Microsoft.CodeAnalysis;
+using Misbat.CodeAnalysis.Test.Extensions;
 
 namespace Misbat.CodeAnalysis.Test.Utility;
 
 [PublicAPI]
 public static class MetadataReferenceUtility
 {
-    public static readonly MetadataReference MsCoreLib = GetAssemblyReference<object>();
+    public static readonly MetadataReference MsCoreLib = FromType<object>();
 
-    public static readonly MetadataReference SystemRuntime = GetAssemblyReference("System.Runtime");
+    public static readonly MetadataReference SystemRuntime = FromName("System.Runtime");
 
-    public static readonly MetadataReference NetStandard = GetAssemblyReference("netstandard");
+    public static readonly MetadataReference NetStandard = FromName("netstandard");
 
-    public static MetadataReference GetAssemblyReference<T>() => MetadataReference.CreateFromFile(typeof(T).Assembly.Location);
-
-    public static MetadataReference GetAssemblyReference(string name)
+    public static MetadataReference FromType<T>()
     {
-        return MetadataReference.CreateFromFile
-        (
-            AppDomain.CurrentDomain.GetAssemblies()
-                .Single(a => a.GetName().Name == name)
-                .Location
-        );
+        Type type = typeof(T);
+        return FromType(type);
     }
+
+    public static MetadataReference FromPath(string path) => MetadataReference.CreateFromFile(path);
+
+    public static MetadataReference FromType(Type type)
+    {
+        Assembly assembly = type.Assembly;
+        string location = assembly.Location;
+        MetadataReference metadataReference = MetadataReference.CreateFromFile(location);
+        return metadataReference;
+    }
+
+    public static MetadataReference FromName(string name)
+    {
+        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+        Assembly assembly = assemblies.Single(a => a.GetName().Name == name);
+        return FromAssembly(assembly);
+    }
+
+    public static MetadataReference FromAssembly(Assembly assembly) => MetadataReference.CreateFromFile(assembly.Location);
+    
+    public static MetadataReference TransitivelyReferenced(Type searchStartType, string name)
+        => TransitivelyReferenced(searchStartType.Assembly, name);
+    
+    public static MetadataReference TransitivelyReferenced(Assembly assembly, string name) => FromAssembly(assembly.FindTransitivelyReferenced(name));
 }
